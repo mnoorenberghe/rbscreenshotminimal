568a718ae10e8d8e839d1793de51d3be271e0eb5
Matthew Noorenberghe <github@matthew.noorenberghe.com>
2014-03-23 20:14:24 -0700

    Initial version of a minimal UI for image attachment review

---
+++ README.md
@@ -0,0 +1,13 @@
+Review Board Minimal Screenshot Review UI
+==============
+
+Review Board extension to make image attachment reviews very simple and hide
+other knobs which aren't important.
+
+This extension auto-publishes image reviews so the draft banner doesn't appear
+and it doesn't redirect users back to the review after doing so.
+
+Buttons to change the status of issues are also hidden.
+
+This extension was written to crowd-source image attachment reviews without
+exposing users to the other features of Review Board.
--- rbscreenshotminimal/static/js/rbscreenshotminimal.js
+++ rbscreenshotminimal/static/js/rbscreenshotminimal.js
@@ -1,5 +1,6 @@
 var RBScreenshotMinimal = {};
 
+// Redirect from the dashboard to the open review list
 if (window.location.pathname == "/dashboard/") {
     window.location = "/r/?show-closed=0";
 }
@@ -10,12 +11,12 @@ RBScreenshotMinimal.Extension = RB.Extension.extend({
         $(document).ready(function() {
             if (RB.DraftReviewBannerView) {
                 RB.DraftReviewBannerView.prototype.show = function() {
-                    console.log("not showing banner and instead auto-publishing");
+                    // not showing banner and instead auto-publishing
                     this._onPublishClicked();
                 };
 
                 RB.DraftReviewBannerView.prototype.hideAndReload = function() {
-                    console.log("not going back to reviewURL");
+                    // not going back to reviewURL
                 };
             }
         });
